32c73479d5ff64f064576b9110fcd9df2ef8489b
Joao Ricardo Simplicio <joaoricardosimplicio16@gmail.com>
2020-06-25 12:38:47 -0300

    update in allowed host list.

--- ranking/settings.py
+++ ranking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '69myfy&cd@1#)upradv^whwmtc(jf$#yfvngkd=t24xk8mp*eg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
