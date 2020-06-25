44ee0a239e6225f64116082e25d9c569ece41b71
Joao Ricardo Simplicio <joaoricardosimplicio16@gmail.com>
2020-06-25 11:44:20 -0300

    add bash to automate extract information process.

---
+++ crawlers.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+python manage.py crawler_nfl
+python manage.py crawler_instagram
+python manage.py crawler_premier_league
+python manage.py crawler_instagram_premier_league
+python manage.py crawler_la_liga
+python manage.py crawler_instagram_la_liga
